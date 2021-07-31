306231eb784e5eed96d235eac12e17fe410e6b97
Evgeniy Sklyarov <skevni@gmail.com>
2021-08-01 00:52:40 +0300

    homework #3

---
+++ src/main/java/ru/gb/sklyarov/shop/controllers/MainController.java
@@ -0,0 +1,13 @@
+package ru.gb.sklyarov.shop.controllers;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Component
+public class MainController {
+
+    @GetMapping
+    public String getStartPage(){
+        return "index.html";
+    }
+}
---
+++ src/main/java/ru/gb/sklyarov/shop/model/Product.java
@@ -0,0 +1,46 @@
+package ru.gb.sklyarov.shop.model;
+
+public class Product {
+    private Long id;
+    private String title;
+    private double cost;
+
+    public Product(Long id, String title, double cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}

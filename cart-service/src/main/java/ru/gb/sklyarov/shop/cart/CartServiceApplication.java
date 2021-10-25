7be8d03589df62e802cb15fb368e54f4b55d4689
Evgeniy Sklyarov <skevni@gmail.com>
2021-10-25 21:27:28 +0300

    homework #11 Spring 2
    renamed

--- cart-service/src/main/java/ru/gb/sklyarov/shop/cart/CartServiceApplication.java
+++ cart-service/src/main/java/ru/gb/sklyarov/shop/cart/CartServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CartApplication {
+public class CartServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CartApplication.class, args);
+        SpringApplication.run(CartServiceApplication.class, args);
     }
 }
--- core-service/src/main/java/ru/gb/sklyarov/shop/core/ms/CoreServiceApplication.java
+++ core-service/src/main/java/ru/gb/sklyarov/shop/core/ms/CoreServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringCoreApplication {
+public class CoreServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringCoreApplication.class, args);
+        SpringApplication.run(CoreServiceApplication.class, args);
     }
 }
